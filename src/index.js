fcb37a1f08191d48a6e83b009e5b9e5a8c2bdfea
alex <sytnikov.zp@gmail.com>
2024-03-11 02:16:38 +0200

    Add emailValid function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@
 
 let email;
 
-const emailInput = document.querySelector('#email');
 const btnOk = document.querySelector('#btn-container > button:last-child');
-
-
+const emailInput = document.querySelector('#email');
 
 emailInput.addEventListener('change', (e) => {
   email = e.target.value;
@@ -13,4 +11,30 @@ emailInput.addEventListener('change', (e) => {
 
 // ----------------------------------------------------------
 
+const divEmail = document.querySelector('.div-email');
+const errormsg = document.createElement('div');
+errormsg.classList.add('error-message');
+errormsg.textContent = 'Wrong email!';
 
+function emailValid(e) {
+  e.preventDefault();
+  const reg = /^\w+\.?\w+@[a-z]{3,8}\.[a-z]{2,5}$/gi;
+  if (reg.test(email)) {
+    emailInput.classList.remove('checkValid_invalid');
+    emailInput.classList.add('checkValid_valid');
+    
+    if (emailInput.nextElementSibling) {
+      emailInput.nextElementSibling.remove();
+    }
+
+    return 'success';
+  }
+
+  emailInput.classList.remove('checkValid_valid');
+  emailInput.classList.add('checkValid_invalid');
+  divEmail.append(errormsg);
+
+  return 'error';
+}
+
+btnOk.addEventListener('click', emailValid);
