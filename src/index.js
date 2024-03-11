8177a86cab6e6fe042cb154d1e0475cadb4b87a2
alex <sytnikov.zp@gmail.com>
2024-03-11 14:14:45 +0200

    Fix function emailValid & revome input style

--- src/index.js
+++ src/index.js
@@ -17,21 +17,13 @@ function emailValid(e) {
   e.preventDefault();
   const reg = /^\w+\.?\w+@[a-z]{3,8}\.[a-z]{2,5}$/gi;
   if (reg.test(email)) {
-    emailInput.classList.remove('checkValid_invalid');
-    emailInput.classList.add('checkValid_valid');
-
     if (emailInput.nextElementSibling) {
       emailInput.nextElementSibling.remove();
     }
-
     return 'success';
   }
-
-  emailInput.classList.remove('checkValid_valid');
-  emailInput.classList.add('checkValid_invalid');
   divEmail.append(errormsg);
-
   return 'error';
 }
 
-emailInput.addEventListener('click', emailValid);
+emailInput.addEventListener('blur', emailValid);
